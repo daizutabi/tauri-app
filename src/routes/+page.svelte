9badda0da1e5c32bc0d76c70abaa637bad43b362
Daizu <daizutabi@gmail.com>
2024-06-25 18:48:26 +0900

    d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,23 +7,20 @@
   let greetMsg = "";
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsg = await invoke("greet", { name });
+  }
+
+  function handleClink(e: MouseEvent) {
+    console.log(e);
   }
 </script>
 
-<div class="m-4 p-4">
-  <h1 class="font-bold text-red-500 text-4xl">Welcome to Tauri!</h1>
-  <ul class="mt-4 p-3 space-y-1 list-disc">
-    <li>HTMLあいう</li>
-    <li>JS Lorem</li>
-    <li>JS</li>
-  </ul>
-
+<div class="m-4 p-4 space-y-2">
   <form on:submit|preventDefault={greet}>
     <Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
     <Button type="submit">Greet</Button>
   </form>
-
   <p>{greetMsg}</p>
+
+  <Button on:click={handleClink}>ほたん</Button>
 </div>
