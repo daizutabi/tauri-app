a24a4499de32ca0165bdc587e0b4c56a8c9e0e99
Daizu <daizutabi@gmail.com>
2024-06-25 18:49:53 +0900

    update

--- src/lib/components/Counter.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  let count: number = 0;
-</script>
-
-<button on:click={() => (count -= 1)} aria-label="減算">-</button>
-{count}
-<button on:click={() => (count += 1)} aria-label="加算">+</button>
